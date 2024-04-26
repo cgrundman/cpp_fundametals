35b2adc5756c8b00c5ed47583f9c66ee4af4ee37
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-26 15:45:46 +0200

    add function and scope files

---
+++ 08-functions/Factorial/main.cpp
@@ -0,0 +1,22 @@
+// Recursion - Factorial
+
+#include <iostream>
+
+using namespace std;
+
+unsigned long long factorial(unsigned long long);
+
+unsigned long long factorial(unsigned long long n) {
+    if (n == 0)
+        return 1;	             // base case
+    return n * factorial(n-1); // recursive case
+}
+
+int main() {
+    int n{20};
+    cout << "Factorial of " << n << ": " << factorial(n) << endl;       // 6
+//    cout << factorial(8) << endl;     // 40320
+//    cout << factorial(12) << endl;   // 479001600
+//    cout << factorial(20) << endl;   // 2432902008176640000
+    return 0;
+}
