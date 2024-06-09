0824292a5a8d2831d9892c3ca67a586c0a103e7f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-09 23:42:01 +0200

    operator overloading initial

---
+++ 11-operator_overloading/Start/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Mystring.h"
+
+using namespace std;
+
+int main() {
+    Mystring empty;              // no-args constructor
+    Mystring larry("Larry");     // overloaded constructor
+    Mystring stooge {larry};     // copy constructor 
+    
+    empty.display();
+    larry.display();
+    stooge.display();
+    
+    return 0;
+}
