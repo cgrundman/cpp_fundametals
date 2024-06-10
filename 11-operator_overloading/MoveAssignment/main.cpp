e6910d2c28b6969bbf591ef561be039f550dd230
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-10 12:25:53 +0200

    add operator methods

--- 11-operator_overloading/MoveAssignment/main.cpp
+++ 11-operator_overloading/MoveAssignment/main.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 int main() {
     
-    Mystring a{"Hello"};                // Overloaded constructor
-    a = Mystring{"Hola"};              // Overloaded constructor then move assignment
-    a = "Bonjour";                         // Overloaded constructor then move assignment
+    Mystring a{"Hello"};              // Overloaded constructor
+    a = Mystring{"Hola"};             // Overloaded constructor then move assignment
+    a = "Bonjour";                    // Overloaded constructor then move assignment
     
  
-    Mystring empty;                      // no-args constructor
-    Mystring larry("Larry");             // overloaded constructor
-    Mystring stooge {larry};            // copy constructor 
-    Mystring stooges;                   // no-args constructor
+    Mystring empty;                   // no-args constructor
+    Mystring larry("Larry");          // overloaded constructor
+    Mystring stooge {larry};          // copy constructor 
+    Mystring stooges;                 // no-args constructor
     
-    empty = stooge;                     // copy assignment operator
-                                                  // stooge is an l-value
+    empty = stooge;                   // copy assignment operator
+                                      // stooge is an l-value
     
-    empty = "Funny";                    // move assignment operator
-                                                  // "Funny" is an r-value
+    empty = "Funny";                  // move assignment operator
+                                      // "Funny" is an r-value
     
     empty.display();
     larry.display();
@@ -32,9 +32,9 @@ int main() {
     stooges.display();
     
     vector<Mystring> stooges_vec;
-    stooges_vec.push_back("Larry");                // Move constructor
-    stooges_vec.push_back("Moe");                // Move constructor
-    stooges_vec.push_back("Curly");              // Move constructor    
+    stooges_vec.push_back("Larry");     // Move constructor
+    stooges_vec.push_back("Moe");       // Move constructor
+    stooges_vec.push_back("Curly");     // Move constructor    
     
     cout << "=== Loop 1 ==================" << endl;
     for (const Mystring &s: stooges_vec) 
@@ -42,7 +42,7 @@ int main() {
         
     cout << "=== Loop 2 ==================" << endl;
     for (Mystring &s: stooges_vec)
-        s = "Changed";                                      // move assignment
+        s = "Changed";                   // move assignment
         
     cout << "=== Loop 3 ==================" << endl;
     for (const Mystring &s: stooges_vec) 
