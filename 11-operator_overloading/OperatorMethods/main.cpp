0f3e88553229545bfdf5042fb08616a064a02629
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-10 12:28:17 +0200

    Update main.cpp

--- 11-operator_overloading/OperatorMethods/main.cpp
+++ 11-operator_overloading/OperatorMethods/main.cpp
@@ -11,24 +11,24 @@ int main() {
     Mystring moe{"Moe"};
     
     Mystring stooge = larry;
-    larry.display();                                                                      // Larry
-    moe.display();                                                                      // Moe
+    larry.display();                                              // Larry
+    moe.display();                                                // Moe
     
-    cout << (larry == moe) << endl;                                         // false
-    cout << (larry == stooge) << endl;                                      // true
+    cout << (larry == moe) << endl;                               // false
+    cout << (larry == stooge) << endl;                            // true
 
-    larry.display();                                                                    // Larry
+    larry.display();                                              // Larry
     Mystring larry2 = -larry;
-    larry2.display();                                                                    // larry
+    larry2.display();                                             // larry
      
-    Mystring stooges = larry + "Moe";                                     // ok with member function
-    //Mystring stooges = "Larry" + moe;                                 // Compiler error
+    Mystring stooges = larry + "Moe";                             // ok with member function
+    //Mystring stooges = "Larry" + moe;                           // Compiler error
     
-    Mystring two_stooges = moe + " " + "Larry";
+    Mystring two_stooges = moe + " " + "Larry";                   // Moe Larry
     two_stooges.display();      
-                                                   // Moe Larry
-    Mystring three_stooges = moe + " " + larry + " " +  "Curly";
-    three_stooges.display();                                                        //  Moe Larry Curly
+                                                   
+    Mystring three_stooges = moe + " " + larry + " " +  "Curly";  //  Moe Larry Curly
+    three_stooges.display();                                                        
     
     return 0;
 }
