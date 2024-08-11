dd7f20fad1bb8737aca20ad484a72dddab921ce7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-08-11 11:47:56 +0200

    create virtual functions

--- Polymorphism/VirtualFunctions/main.cpp
+++ Polymorphism/VirtualFunctions/main.cpp
@@ -1,7 +1,6 @@
-// Using Base class pointers
+// Virtual functions
 
 #include <iostream>
-#include <vector>
 
 // This class uses dynamic polymorphism for the withdraw method
 class Account {
@@ -9,7 +8,6 @@ public:
     virtual void withdraw(double amount) {
         std::cout << "In Account::withdraw" << std::endl;
     }
-    virtual ~Account() {  }
 };
 
 class Checking: public Account  {
@@ -17,8 +15,6 @@ public:
     virtual void withdraw(double amount) {
         std::cout << "In Checking::withdraw" << std::endl;
     }
-    
-    virtual ~Checking() {  }
 };
 
 class Savings: public Account  {
@@ -26,7 +22,6 @@ public:
     virtual void withdraw(double amount) {
         std::cout << "In Savings::withdraw" << std::endl;
     }
-    virtual ~Savings() {  }
 };
 
 class Trust: public Account  {
@@ -34,7 +29,6 @@ public:
     virtual void withdraw(double amount) {
         std::cout << "In Trust::withdraw" << std::endl;
     }
-    virtual ~Trust() {  }
 };
 
 int main() {
@@ -49,26 +43,6 @@ int main() {
     p3->withdraw(1000);
     p4->withdraw(1000);
     
-    std::cout << "\n === Array ==== " << std::endl;
-    Account *array [] = {p1,p2,p3,p4};
-    for (auto i=0; i<4; ++i)
-        array[i]->withdraw(1000);
-        
-    std::cout << "\n === Array ==== " << std::endl;
-    array[0] = p4;
-    for (auto i=0; i<4; ++i)
-        array[i]->withdraw(1000);
-
-    std::cout << "\n === Vector ==== " << std::endl;
-    std::vector<Account *> accounts {p1,p2,p3,p4};
-    for (auto acc_ptr: accounts)
-        acc_ptr->withdraw(1000);
-        
-    std::cout << "\n === Vector ==== " << std::endl;
-    accounts.push_back(p4);
-    accounts.push_back(p4);
-    for (auto acc_ptr: accounts)
-        acc_ptr->withdraw(1000);
 
     std::cout << "\n === Clean up ==== " << std::endl;
     delete p1;
