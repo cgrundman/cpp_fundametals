3633c940852c53bfd2c7a3082eea133633ba6492
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-08-18 15:50:19 +0200

    finalize solution to code

--- 13-polymorphism/Challange/main.cpp
+++ 13-polymorphism/Challange/main.cpp
@@ -10,77 +10,27 @@
 using namespace std; 
 
 int main() {
+    Checking_Account frank {"Frank", 5000};
+    cout << frank << endl;
     
-//    Account joe;
-//    Checking_Account c;
-//    cout << c << endl;
-//    
-//    Savings_Account s {"Frank", 5000, 2.6};
-//    cout << s << endl;
-//    s.deposit(10000);
-//    cout << s << endl;
-//    s.withdraw(10000);
-//    cout << s << endl;
+    Account *trust = new Trust_Account("James");
+    cout << *trust << endl;
     
-    Account *ptr = new Trust_Account("Leo", 10000, 2.6);
-    cout << *ptr << endl;
+    Account *p1 = new Checking_Account("Larry", 10000);
+    Account *p2 = new Savings_Account("Moe", 1000);
+    Account *p3 = new Trust_Account("Curly");
     
-//    cout.precision(2);
-//    cout << fixed;
-//   
-//    // Accounts
-//    vector<Account> accounts;
-//    accounts.push_back(Account {});
-//    accounts.push_back(Account {"Larry"});
-//    accounts.push_back(Account {"Moe", 2000} );
-//    accounts.push_back(Account {"Curly", 5000} );
-//    
-//    display(accounts);
-//    deposit(accounts, 1000);
-//    withdraw(accounts,2000);
-//    
-//    // Savings 
-//
-//    vector<Savings_Account> sav_accounts;
-//    sav_accounts.push_back(Savings_Account {} );
-//    sav_accounts.push_back(Savings_Account {"Superman"} );
-//    sav_accounts.push_back(Savings_Account {"Batman", 2000} );
-//    sav_accounts.push_back(Savings_Account {"Wonderwoman", 5000, 5.0} );
-//
-//    display(sav_accounts);
-//    deposit(sav_accounts, 1000);
-//    withdraw(sav_accounts,2000);
-//   
-//   // Checking
-//   
-//    vector<Checking_Account> check_accounts;
-//    check_accounts.push_back(Checking_Account {} );
-//    check_accounts.push_back(Checking_Account {"Kirk"} );
-//    check_accounts.push_back(Checking_Account {"Spock", 2000} );
-//    check_accounts.push_back(Checking_Account {"Bones", 5000} );
-//
-//    display(check_accounts);
-//    deposit(check_accounts, 1000);
-//    withdraw(check_accounts, 2000);
-//
-//    // Trust
-//  
-//    vector<Trust_Account> trust_accounts;
-//    trust_accounts.push_back(Trust_Account {} );
-//    trust_accounts.push_back(Trust_Account {"Athos", 10000, 5.0} );
-//    trust_accounts.push_back(Trust_Account {"Porthos", 20000, 4.0} );
-//    trust_accounts.push_back(Trust_Account {"Aramis", 30000} );
-//
-//    display(trust_accounts);
-//    deposit(trust_accounts, 1000);
-//    withdraw(trust_accounts, 3000);
-//    
-//    // Withdraw 5 times from each trust account
-//    // All withdrawals should fail if there are too many withdrawals or if the withdrawl is > 20% of the balance
-//    for (int i=1; i<=5; i++)
-//        withdraw(trust_accounts, 1000);
-//    
-//
-//    
+    std::vector<Account *> accounts {p1,p2,p3};
+    
+    display(accounts);
+    deposit(accounts, 1000);
+    withdraw(accounts, 2000);
+    
+    display(accounts);
+    
+    delete p1;
+    delete p2;
+    delete p3;
+    
     return 0;
 }
